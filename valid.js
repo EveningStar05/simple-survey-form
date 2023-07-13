6dc37cb9ff27ee251cc8cd7ee3d649089983a06f
maitreya <bluespider529@gmail.com>
2023-07-13 23:46:30 +0700

    added custom form validation: error message for when input field is empty

--- valid.js
+++ valid.js
@@ -1,31 +1,42 @@
-// pseudocode:
-// check for empty input before submitting.
-// then check one by one input type
+const form = document.querySelector("form")
+const set_default_msg = document.querySelector("span.error-message");
 
 function formValidation() {
-    const form = document.getElementById("survey-form") // get the form
-    const input = document.querySelectorAll("input"); // get all input
-    const errorMessage = document.getElementsByClassName("error-message") // get the error message element
+    let text_input = document.querySelectorAll("input:not(#submit), select, textarea");
 
-    // check for empty input
-    form.addEventListener("submit", (Event) => {
-        for(let i = o; i < input.length; i++) {
-            // incase if there is error, if the field is valid
-            // then we want to reset the error message
-            if (input[i].validity.valid) {
-                errorMessage.textContent = "";
-                errorMessage.className = "error-message"
-            } else {
-                showError(input[i])
-            }
+    for (let i = 0; i < text_input.length; i++) {
+        if (text_input[i].validity.valid) {
+            set_default_msg.textContent = "";
+        } else {
+            showError(text_input[i])
         }
-    })
-}
-
-function showError(element) {
-    if(element.validity.valueMissing) {
-        errorMessage.textContent = "You must fill in the input field!"
     }
 }
 
-formValidation()
+function showError(element) {
+
+    let element_name;
+
+    if(element.name === "rd-age-group") {
+        element_name = "div.age-group + span.error-message"
+    } else if (element.id === "#textarea") {
+        element_name = `#${element.id} span.error-message` 
+    } else {
+        element_name = `#${element.id} + span.error-message` 
+    }
+
+    const errorMessage = document.querySelector(element_name) // get the error message element
+
+    if(element.validity.valueMissing) { // check for empty input field
+        if(element.id === "dropdown" || element.name === "rd-age-group") {
+            errorMessage.textContent = "Please select one of the answer"
+        } else {
+            errorMessage.textContent = "You must fill in the blank field.";
+        }
+    }
+}
+
+form.addEventListener("submit", (Event) => {
+    Event.preventDefault() // prevents the page to reload.
+    formValidation()
+})
